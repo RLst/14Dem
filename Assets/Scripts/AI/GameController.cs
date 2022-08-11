8bccf07b55e3cd481355906f5d8226f3a0a514cb
Tony Le <letony@icloud.com>
2022-08-11 15:35:34 +1000

    Begin game controller

---
+++ Assets/Scripts/AI/GameController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace LeMinhHuy.AI
+{
+	public class GameController : MonoBehaviour
+	{
+		void Awake()
+		{
+
+		}
+		void Start()
+		{
+
+		}
+		void InitTeams()
+		{
+			
+		}
+		void BeginMatch()
+		{
+
+		}
+		void EndMatch()
+		{
+
+		}
+	}
+}
